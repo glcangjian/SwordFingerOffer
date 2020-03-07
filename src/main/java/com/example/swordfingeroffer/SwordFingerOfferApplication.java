74470aabde71790759ea4154a5173f377802b845
cangjian <m15927374217@163.com>
2020-03-07 15:54:15 +0800

    项目初始化

---
+++ src/main/java/com/example/swordfingeroffer/SwordFingerOfferApplication.java
@@ -0,0 +1,13 @@
+package com.example.swordfingeroffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordFingerOfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwordFingerOfferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swordfingeroffer/SwordFingerOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swordfingeroffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwordFingerOfferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
